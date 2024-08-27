b5449f5fe11cc0840b5733e1cc625fcb9470e343
Anza15 <mukhiithianza1@gmail.com>
2024-08-27 15:14:54 +0200

    updated my files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import logo from './logo.svg';
-import Weather from './Weather';
-import axios from 'axios';
 import './App.css';
-import WeatherForecast from './WeatherForecast';
-import WeatherForecastt from './WeatherForecastt';
 
-
-export default function App() {
+function App() {
   return (
     <div className="App">
-       <div className="container">
-      
-       <h1>Weather App</h1>
-       <Weather defaultCity="Pretoria"/>
-      
-          <footer>
-            This project was coded by {""}
-            <a href="#" target="_blank"
-            rel="noopener noreferrer">
-            Anza Mukhithi
-          </a>{""}
-          and is {""}
-          <a href="https://github/Anza15/search-enginee" target="_blank"
-          rel="noopener noreferrer">
-            open-source on Github
-
-            and {""}
-            <a
-            href="https://.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
-  
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-      </footer>
-      </div>
-      </div>
+      </header>
+    </div>
   );
 }
 
+export default App;
